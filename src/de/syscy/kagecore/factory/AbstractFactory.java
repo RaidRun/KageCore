9eb400235f3bd23d1daf9163f854da8ca273640e
MagmaMusen <MagmaMusen@gmail.com>
2022-03-22 13:26:17 +0100

    bug: fixed reloading recursive bug only using last folder

--- src/de/syscy/kagecore/factory/AbstractFactory.java
+++ src/de/syscy/kagecore/factory/AbstractFactory.java
@@ -1,5 +1,6 @@
 package de.syscy.kagecore.factory;
 
+import de.syscy.kagecore.KageCore;
 import lombok.Getter;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -8,46 +9,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractFactory<T> implements IFactory<T> {
-	private File folder;
-	private String templateFileExtension;
+    private File folder;
+    private String templateFileExtension;
 
-	protected @Getter Map<String, IFactoryTemplate<T>> templates = new HashMap<>();
+    protected @Getter
+    Map<String, IFactoryTemplate<T>> templates = new HashMap<>();
 
-	protected void loadTemplates(File folder, final String templateFileExtension) {
-		this.folder = folder;
-		this.templateFileExtension = templateFileExtension;
+    private void loadTemplatesRecursive(File folder, final String templateFileExtension) {
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
 
-		if(!folder.exists()) {
-			folder.mkdirs();
-		}
+        File[] files = folder.listFiles(file -> file.isDirectory() || file.getName().endsWith("." + templateFileExtension));
 
-		File[] files = folder.listFiles(file -> file.isDirectory() || file.getName().endsWith("." + templateFileExtension));
+        for (File file : files != null ? files : new File[0]) {
+            if (file.isDirectory()) {
+                loadTemplates(file, templateFileExtension);
+            } else {
+                YamlConfiguration templateYamlFile = YamlConfiguration.loadConfiguration(file);
+                String templateName = file.getName().substring(0, file.getName().indexOf('.')).toLowerCase();
 
-		for(File file : files != null ? files : new File[0]) {
-			if(file.isDirectory()) {
-				loadTemplates(file, templateFileExtension);
-			} else {
-				YamlConfiguration templateYamlFile = YamlConfiguration.loadConfiguration(file);
-				String templateName = file.getName().substring(0, file.getName().indexOf('.')).toLowerCase();
+                try {
+                    IFactoryTemplate<T> template = createTemplate();
+                    template.load(this, templateName, templateYamlFile);
 
-				try {
-					IFactoryTemplate<T> template = createTemplate();
-					template.load(this, templateName, templateYamlFile);
+                    templates.put(templateName, template);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 
-					templates.put(templateName, template);
-				} catch(Exception ex) {
-					ex.printStackTrace();
-				}
-			}
-		}
-	}
+    protected void loadTemplates(File folder, final String templateFileExtension) {
+        this.folder = folder;
+        this.templateFileExtension = templateFileExtension;
+        loadTemplatesRecursive(folder, templateFileExtension);
+    }
 
-	@Override
-	public void reload() {
-		templates.clear();
 
-		loadTemplates(folder, templateFileExtension);
-	}
+    @Override
+    public void reload() {
+        templates.clear();
+        KageCore.debugMessage("running reload in abstract factory: " + folder.getPath());
+        loadTemplates(folder, templateFileExtension);
+    }
 
-	protected abstract IFactoryTemplate<T> createTemplate();
+    protected abstract IFactoryTemplate<T> createTemplate();
 }
