2a90df64b9d7aa0e948e99328f0c454956ad79eb
Einar Jörgen Hansson <einarjorgen.hansson@gmail.com>
2022-05-10 12:31:38 +0200

    feat: added initial test case

---
+++ test/unit/de/syscy/kagecore/KageCoreTest.java
@@ -0,0 +1,19 @@
+package unit.de.syscy.kagecore;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KageCoreTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onEnable() {
+        assert(true);
+    }
+}
